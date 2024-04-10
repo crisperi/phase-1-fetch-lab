ca271a85504570ab1c50736c69bba29a1b3d57ac
crisperi <phoenixedian@gmail.com>
2024-04-11 01:51:01 +0300

    done

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => {
+    const fifthBook = json[4];
+    console.log(fifthBook);
+    renderBooks(json)
+    renderBooks([fifthBook]);
+  })
   
 }
 
